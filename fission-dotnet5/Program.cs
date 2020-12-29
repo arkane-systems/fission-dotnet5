41edac1768ebec4043b9cee3b9314723d432be15
Alistair Young <avatar@arkane-systems.net>
2020-12-29 11:39:42 -0600

    Use C# 9 top-level code.

--- fission-dotnet5/Program.cs
+++ fission-dotnet5/Program.cs
@@ -1,28 +1,21 @@
 #region using
 
+using Fission.DotNet;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 
 #endregion
 
-namespace Fission.DotNet
-{
-    public static class Program
-    {
-        public static int Main (string[] args)
-        {
-            // Build minimal web host.
-            IWebHost host = new WebHostBuilder ()
-                           .ConfigureLogging (configureLogging: log => log.AddConsole ())
-                           .UseKestrel ()
-                           .UseUrls ("http://*:8888")
-                           .UseStartup<Startup> ()
-                           .Build ();
+// Build minimal web host.
+IWebHost host = new WebHostBuilder ()
+               .ConfigureLogging (configureLogging: log => log.AddConsole ())
+               .UseKestrel ()
+               .UseUrls ("http://*:8888")
+               .UseStartup<Startup> ()
+               .Build ();
 
-            host.Run ();
+host.Run ();
 
-            // Return success.
-            return 0;
-        }
-    }
-}
+// Return success.
+return 0;
