e47081d7c3a50c1a2247d6bdbbe6f9d91211d567
Alistair Young <avatar@arkane-systems.net>
2020-12-30 15:17:49 -0600

    Added doccomments to Fission.Functions.

--- Fission.Functions/FissionRequest.cs
+++ Fission.Functions/FissionRequest.cs
@@ -19,22 +19,49 @@ using JetBrains.Annotations;
 
 namespace Fission.Functions
 {
+    /// <summary>
+    ///     Underlying request details supplied to the handling function.
+    /// </summary>
     [PublicAPI]
     public record FissionRequest
     {
+        /// <summary>
+        ///     The body of the request, supplied as a readable <see cref="Stream" />. See also
+        ///     <seealso cref="FissionRequest.GetBodyAsString" /> to receive the entire body of the request as a single
+        ///     <see cref="string" />.
+        /// </summary>
         public Stream Body;
 
+        /// <summary>
+        ///     The client certificate supplied with the request.
+        /// </summary>
         [CanBeNull]
         public X509Certificate2 ClientCertificate;
 
+        /// <summary>
+        ///     The HTTP headers supplied with the request.
+        /// </summary>
         public IReadOnlyDictionary<string, IEnumerable<string>> Headers;
 
+        /// <summary>
+        ///     The HTTP method used to make the request (i.e., "GET", "POST", etc.).
+        /// </summary>
         public string Method;
 
+        /// <summary>
+        ///     The URL used to make the request, formatted as a string.
+        /// </summary>
+        /// <remarks>
+        ///     The URL is urlencoded, and includes all elements, including (for example) the query string.
+        /// </remarks>
         public string Url;
 
+        /// <summary>
+        ///     Get the body of the request as a single <see cref="string" />.
+        /// </summary>
+        /// <returns>The body of the request as a single <see cref="string" />.</returns>
         [NotNull]
-        public string BodyAsString ()
+        public string GetBodyAsString ()
         {
             var length = (int) this.Body.Length;
             var data   = new byte[length];
